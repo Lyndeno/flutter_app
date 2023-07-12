efc6d31e2d1c52cee16734987bab4354f50e29f5
Lyndon Sanche <lsanche@lyndeno.ca>
2023-07-11 19:12:39 -0600

    flake: Make package work

--- flake.nix
+++ flake.nix
@@ -8,6 +8,13 @@
     };
     
   in {
+    packages."x86_64-linux".default = pkgs.flutter.buildFlutterApplication {
+      version = "0.0.1";
+      name = "flutter_app";
+      src = self;
+      vendorHash = "sha256-JXLv+3+xega5umGoQs95+wnMrL69DdJPHnTjmjk7fz0=";
+      autoDepsList = true;
+    };
     devShells."x86_64-linux".default = pkgs.mkShell {
       buildInputs = [ pkgs.flutter ];
     };
