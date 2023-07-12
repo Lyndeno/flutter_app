efaf61c27fdc7a37f6e049a447389a6669d0d03c
Lyndon Sanche <lsanche@lyndeno.ca>
2023-07-11 20:59:32 -0600

    Early android support, no working though

---
+++ android.nix
@@ -0,0 +1,19 @@
+{ androidenv }:
+
+androidenv.composeAndroidPackages {
+  toolsVersion = "26.1.1";
+  platformToolsVersion = "33.0.3";
+  buildToolsVersions = [ "30.0.3" ];
+  includeEmulator = false;
+  emulatorVersion = "31.3.10";
+  platformVersions = [ "31" "32" "33"];
+  includeSources = false;
+  includeSystemImages = false;
+  systemImageTypes = [ "google_apis_playstore" ];
+  abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+  cmakeVersions = [ "3.10.2" ];
+  includeNDK = true;
+  ndkVersions = [ "22.0.7026061" ];
+  useGoogleAPIs = false;
+  useGoogleTVAddOns = false;
+}
--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
   outputs = { self, nixpkgs}: let
     pkgs = import nixpkgs {
       system = "x86_64-linux";
+      config.allowUnfree = true;
+      config.android_sdk.accept_license = true;
     };
     
   in {
@@ -15,8 +17,20 @@
       vendorHash = "sha256-JXLv+3+xega5umGoQs95+wnMrL69DdJPHnTjmjk7fz0=";
       autoDepsList = true;
     };
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = [ pkgs.flutter ];
+    devShells."x86_64-linux".default = let
+      android = pkgs.callPackage ./android.nix {};
+      in pkgs.mkShell {
+      buildInputs = [
+        pkgs.flutter
+
+        # For android
+        pkgs.jdk17
+        android.platform-tools
+      ];
+
+      ANDROID_HOME = "${android.androidsdk}/libexec/android-sdk";
+      JAVA_HOME = pkgs.jdk17;
+      ANDROID_AVD_HOME = (toString ./.) + "/.android/avd";
     };
   };
 }
